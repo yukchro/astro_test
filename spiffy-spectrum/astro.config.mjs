24569885e4c26c092c00a1885e39229ae15ca862
yasuyuki.kuroda <yasuyuki.kuroda@gpol.co.jp>
2023-05-30 12:50:22 +0900

    create: common component sample

--- spiffy-spectrum/astro.config.mjs
+++ spiffy-spectrum/astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import installedIntegration from '@astrojs/vue';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    installedIntegration(),
+  ]
+});
---
+++ spiffy-spectrum/src/components/Gheader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="l-gheader">
+    <button class="l-gheader__button" @click="status = !status">
+      <span v-if="status" class="l-gnav-header__menu is-active">Close</span>
+      <span v-else class="l-gnav-header__menu">Menu</span>
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    status: Boolean
+  },
+  data() {
+    return {
+      status: false
+    }
+  }
+}
+
+</script>
