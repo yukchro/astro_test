068f459b6b60aec8475928a80a8e264b0099f487
yasuyuki.kuroda <yasuyuki.kuroda@gpol.co.jp>
2023-09-01 13:40:24 +0900

    [update] demo page tweak, create introduction app

---
+++ spiffy-spectrum/src/components/modal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="c-modal">
+    モーダル
+    <span>{{modalData.number}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modalData: Object
+  }
+}
+</script>
