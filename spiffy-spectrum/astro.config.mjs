d6757462d0ac5e28a212b8bc36d29ec20bf82c64
yasuyuki.kuroda <yukchro@gmail.com>
2024-10-01 13:51:21 +0900

    deploy setting

--- spiffy-spectrum/astro.config.mjs
+++ spiffy-spectrum/astro.config.mjs
@@ -3,6 +3,8 @@ import installedIntegration from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yukchro.github.io',
+  base: 'astro_test',
   integrations: [
     installedIntegration(),
   ]
